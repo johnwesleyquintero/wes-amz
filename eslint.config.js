cb4a96598d28ec4b57a52065e997f1362dc35083
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-06-22 14:37:10 +0800

    chore: update 3 files

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -10,20 +11,40 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
     },
     plugins: {
+      react: react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules, // Add react recommended rules here
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "react/react-in-jsx-scope": "off", // Disable the rule for React 17+
+      "react/no-unescaped-entities": "off", // Disable the rule for unescaped entities
+      "react/prop-types": "off", // Disable prop-types validation for TypeScript projects
+      "react/no-unknown-property": "off", // Disable unknown property warnings
+    },
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
     },
   },
 );
