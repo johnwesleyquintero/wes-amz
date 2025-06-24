f7b32e0b94d3c821d353d3d01ba1dcbc5346aa7f
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-06-24 16:05:26 +0800

    refactor(layout): remove unused Link import from TopBar

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+
 import { Bell, Search, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useIsMobile } from "@/hooks/use-mobile";
